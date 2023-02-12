d04a507046172b9d5704286b86ebedf14376ce43
jaiden <j7891@protonmail.com>
2023-02-12 00:44:18 +0000

    added register dictionary

--- main.py
+++ main.py
@@ -1,40 +1,64 @@
-with open('temp.txt', "r+b") as f:
+registers={
+	b'\xf0\x9f\x8d\x8c': None,
+	b'\xf0\x9f\xa7\x83': None,
+	b'\xf0\x9f\xa5\x91': None,
+	b'\xf0\x9f\xa9\xb3': None
+}
+
+with open('test.cn', "r+b") as f:
     line = f.readline()
     lexed = []
     for i in range(len(line) // 4):
         lexed.append(line[4 * i: 4 * i + 4])
-        
-    if lexed[0] == b'\xf0\x9f\x8d\x8e':
-        #1🍎
-    if lexed[0] == b'\xf0\x9f\x8c\x88':
-        #2🌈
-    if lexed[0] == b'\xf0\x9f\x8d\x89':
-        #3🍉
-    if lexed[0] == b'\xf0\x9f\x8c\xba':
-        #4🌺
-    if lexed[0] == b'\xf0\x9f\xa5\x9d':
-        #5🥝
-    if lexed[0] == b'\xf0\x9f\x8d\x87':
-        #6🍇
-    if lexed[0] == b'\xf0\x9f\x8d\x8a':
-        #7🍊
-    if lexed[0] == b'\xf0\x9f\x8d\x93':
-        #8🍓
-    if lexed[0] == b'\xf0\x9f\x8d\x92':
-        #9🍒
-    if lexed[0] == b'\xf0\x9f\x8d\x88':
-        #10🍈
-    if lexed[0] == b'\xf0\x9f\x8d\x8d':
-        #11⛱️
-    if lexed[0] == b'\xf0\x9f\x8d\xb9':
-        #12⛵️
-    if lexed[0] == b'\xf0\x9f\xa5\xad':
-        #13🥭
-    if lexed[0] == b'\xf0\x9f\xa4\xbf':
-        #14🤿
-    if lexed[0] == b'\xf0\x9f\x8f\x9d':
-        #15🏝 
-    if lexed[0] == b'\xf0\x9f\x8c\x8b':
-        #16🌋 
-    if lexed[0] == b'\xf0\x9f\x8c\x8a':
-        #17🌊
+
+if lexed[0] == b'\xf0\x9f\x8d\x8e':
+	#1🍎
+	print()
+if lexed[0] == b'\xf0\x9f\x8c\x88':
+	#2🌈
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x89':
+	#3🍉
+	print()
+if lexed[0] == b'\xf0\x9f\x8c\xba':
+	#4🌺
+	print()
+if lexed[0] == b'\xf0\x9f\xa5\x9d':
+	#5🥝
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x87':
+	#6🍇
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x8a':
+	#7🍊
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x93':
+	#8🍓
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x92':
+	#9🍒
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x88':
+	#10🍈
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\x8d':
+	#11⛱️
+	print()
+if lexed[0] == b'\xf0\x9f\x8d\xb9':
+	#12⛵️
+	print()
+if lexed[0] == b'\xf0\x9f\xa5\xad':
+	#13🥭
+	print()
+if lexed[0] == b'\xf0\x9f\xa4\xbf':
+	#14🤿
+	print()
+if lexed[0] == b'\xf0\x9f\x8f\x9d':
+	#15🏝
+	print()
+if lexed[0] == b'\xf0\x9f\x8c\x8b':
+	#16🌋 
+	print()
+if lexed[0] == b'\xf0\x9f\x8c\x8a':
+	#17🌊
+	print()
