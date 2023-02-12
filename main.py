03ecd74e84058247ef082b83d7be603be5a6dc7f
tienbui2106 <buinhattien216@gmail.com>
2023-02-12 00:04:39 -0600

    update main.py

--- main.py
+++ main.py
@@ -12,54 +12,8 @@ registers = {
 }
 
 
-def eq_op():
-	# syntax: [EQ_EMOJ][R_src][R_src][R_dest]
-	if registers[lexed[1]] == registers[lexed[2]]:
-		registers[lexed[3]] = True
-	else:
-		registers[lexed[3]] = False
-	print(registers[lexed[3]])
-
-
-def lt_op():
-	# syntax: [LT_EMOJ][R_src][R_src][R_dest]
-	if registers[lexed[1]] < registers[lexed[2]]:
-		registers[lexed[3]] = True
-	else:
-		registers[lexed[3]] = False
-	print(registers[lexed[3]])
-
-
-def gt_op():
-	# syntax: [GT_EMOJ][R_src][R_src][R_dest]
-	if registers[lexed[1]] > registers[lexed[2]]:
-		registers[lexed[3]] = True
-	else:
-		registers[lexed[3]] = False
-	print(registers[lexed[3]])
-
-
-def not_op():
-	# syntax: [NOT_EMOJ][R_src][R_dest]
-	registers[lexed[2]] = not registers[lexed[1]]
-	print(registers[lexed[2]])
-
-
-def or_op():
-	# syntax: [OR_EMOJ][R_src][R_src][R_dest]
-	registers[lexed[3]] = registers[lexed[1]] or registers[lexed[2]]
-	print(registers[lexed[3]])
-
-
-def and_op():
-	# syntax: [AND_EMOJ][R_src][R_src][R_dest]
-	registers[lexed[3]] = registers[lexed[1]] and registers[lexed[2]]
-	print(registers[lexed[3]])
-
-
 with open('test.cn', "r+b") as f:
 	lines = f.readlines()
-	if_flag = False
 	for line in lines:
 		lexed = [line[i:i + 4] for i in range(0, len(line), 4)]
 
@@ -81,11 +35,9 @@ with open('test.cn', "r+b") as f:
 		if lexed[0] == b'\xf0\x9f\x8d\x87':
 			# 6🍇 Addition Operator
 			registers[lexed[3]] = registers[lexed[1]] + registers[lexed[2]]
-			print(registers[lexed[3]])
 		if lexed[0] == b'\xf0\x9f\x8d\x8a':
 			# 7🍊multiplication operator
 			registers[lexed[3]] = registers[lexed[1]] * registers[lexed[2]]
-			print(registers[lexed[3]])
 		if lexed[0] == b'\xf0\x9f\x8d\x93':
 			# 8🍓Subtraction Operator
 			registers[lexed[3]] = registers[lexed[1]] - registers[lexed[2]]
@@ -96,28 +48,32 @@ with open('test.cn', "r+b") as f:
 			print(registers[lexed[3]])
 		if lexed[0] == b'\xf0\x9f\x8d\x88':
 			# 10🍈 Equal operator
-			eq_op()
+			registers[lexed[3]] = registers[lexed[1]] == registers[lexed[2]]
 		if lexed[0] == b'\xf0\x9f\x8d\x8d':
 			# 11🍍NOT operator
-			not_op()
+			registers[lexed[2]] = not registers[lexed[1]]
 		if lexed[0] == b'\xf0\x9f\x8d\xb9':
 			# 12🍹 OR operator
-			or_op()
+			registers[lexed[3]] = registers[lexed[1]] or registers[lexed[2]]
 		if lexed[0] == b'\xf0\x9f\xa5\xad':
 			# 13🥭AND operator
-			and_op()
+			registers[lexed[3]] = registers[lexed[1]] and registers[lexed[2]]
 		if lexed[0] == b'\xf0\x9f\xa4\xbf':
 			# 14🤿 > operator
-			gt_op()
+			registers[lexed[3]] =  registers[lexed[1]] > registers[lexed[2]]
 		if lexed[0] == b'\xf0\x9f\x8f\x9d':
 			# 15🏝 < operator
-			lt_op()
+			registers[lexed[3]] = registers[lexed[1]] < registers[lexed[2]]
 		if lexed[0] == b'\xf0\x9f\x8c\x8b':
 			# 16🌋
 			pass
 		if lexed[0] == b'\xf0\x9f\x8c\x8a':
 			# 17🌊
 			pass
+		if lexed[0] == b'\xf0\x9f\x8e\xa3':
+			#18🎣 Print statement
+			if lexed[1] in registers: 
+				print(registers[lexed[1]])
 		if lexed[0] in registers:
 			registers[lexed[0]] = eval(b''.join(lexed[i]
 								for i in range(1, len(lexed))))
